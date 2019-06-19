260fdc674bd98b5e41f892845fdc81b8208d02d0
Emile Silas SARE <emile.silas@gmail.com>
2019-06-19 19:24:48 +0100

    deleted file

--- types/mixins/oz.d.ts
+++
@@ -1,5 +0,0 @@
-import OWebApp from "../OWebApp";
-import { tFileQuality } from "../OWebFS";
-export default function (app: OWebApp): {
-    oz_file_link: (file: string, quality?: tFileQuality, def?: string | undefined) => string;
-};
